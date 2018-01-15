ffedcdca74895b96f690508deacd3f5f9735bd84
Khorram Khondkar <khorramk.kbsk@gmail.com>
2018-01-15 21:36:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(obj, key, value){
-  Object.assign(obj, key, value);
-  return object;
+  
+  return Object.assign({}, obj, { [key]: value });
 }
